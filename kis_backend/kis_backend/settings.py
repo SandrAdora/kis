aa9fd961f6dc54f206669ea90c50fe140cac7f00
SandrAdora <sandraedigin@yahoo.com>
2026-04-14 12:41:09 +0200

    updated files

--- kis_backend/kis_backend/settings.py
+++ kis_backend/kis_backend/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'kis_backend.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+        default=os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
         conn_max_age=600,
     )
 }
